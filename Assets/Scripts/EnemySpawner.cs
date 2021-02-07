a631c812e280fbeac1520b200f71375da1065e06
yradwan147 <54478004+yradwan147@users.noreply.github.com>
2021-02-08 00:06:53 +0200

    Add files via upload

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public GameObject enemyPrefab;
+	public float speed = 15.0f;
+	public float width = 10f;
+	public float height = 5f;
+	public float padding = 7f;
+	Vector3 x = Vector3.left;
+	float xmin;
+	float xmax;
+	// Use this for initialization
+	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding; 
+		
+		foreach( Transform child in transform){
+			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
+	}
+	
+	public void OnDrawGizmos(){
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += x * speed * Time.deltaTime;
+		float nwX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		if (nwX == xmin || nwX == xmax){
+			if (x == Vector3.right){
+				x = Vector3.left;
+			}else if(x == Vector3.left){
+				x = Vector3.right;
+			}
+		}
+		transform.position = new Vector3(nwX, transform.position.y,transform.position.z);
+			
+		}
+	}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public GameObject laserPrefab;
+	public float speed = 15.0f;
+	public float padding = 1f;
+	float lastStep, timeBetweenSteps = 0.5f;
+	float xmin;
+	float xmax;
+	float ymax;
+	
+	void Start(){
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+		Vector3 upmost = Camera.main.ViewportToWorldPoint(new Vector3(0,1,distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x - padding;
+		ymax = upmost.y -padding; 
+				
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			//transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		} else if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector3(newX, transform.position.y,transform.position.z);
+		
+		if (Input.GetKey(KeyCode.Space)){
+			if(Time.time - lastStep > timeBetweenSteps){
+				lastStep = Time.time;
+				GameObject laser = Instantiate(laserPrefab,transform.position,Quaternion.identity) as GameObject;
+				laser.transform.parent = transform;
+		}
+		//foreach(Transform child in transform){
+			//while(true){
+			//child.transform.position += Vector3.up * Time.deltaTime;
+			//if (child.transform.position.y >= ymax){
+				//Destroy(child);
+			//}
+		}
+		}
+	}
